aaa12954ce7421ba525ba57e4b929b2327eb002c
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2023-11-22 13:35:22 +0530

    Update Bard.py

--- Bard.py
+++ Bard.py
@@ -1,15 +1,83 @@
 from bardapi import BardCookies
 import datetime
+import pyperclip
+import pyautogui
+import webbrowser
+from time import sleep
+import json
+import keyboard
 
-cookie_dict={
-    "__Secure-1PSID" : "dAjjSvL07uQ4hcJQrjK3hEIpIK36ctHqUwXK2Y4tCpZqrvWgGYd_n1ntKJuxb9jlCIdwFw.",
-    "__Secure-1PSIDTS" : "sidts-CjEBNiGH7q5mF7mPUvJ4V28iXtQRXoO4HD1jx1ezAmfFnV19BG2U-RbsbZwSj4nDoCVnEAA",
-    "__Secure-1PSIDCC" : "ACA-OxOaXYchG7xmUy7vRRtPe-RMEhTv9wstGLfeobBi3EdykIZs_nV5hBgKcZOrNBGaZVQFGhY"
-}
+def CookieScrapper():
+    webbrowser.open("https://bard.google.com")
+    sleep(5)
+    pyautogui.click(x=1721, y=73)
+    sleep(1)
+    pyautogui.click(x=1471, y=253)
+    sleep(1)
+    pyautogui.click(x=1405, y=115)
+    sleep(1)
+    keyboard.press_and_release('ctrl + w')
+
+    data = pyperclip.paste()
+
+    try:
+        json_data = json.loads(data)
+        pass
+
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON data: {e}")
+
+    SID = "__Secure-1PSID"
+    TS = "__Secure-1PSIDTS"
+    CC = "__Secure-1PSIDCC"
+
+    SIDValue = next((item for item in json_data if item["name"] == SID), None)
+    TSValue = next((item for item in json_data if item["name"] == TS), None)
+    CCValue = next((item for item in json_data if item["name"] == CC), None)
+
+    if SIDValue is not None:
+        SIDValue = SIDValue["value"]
+    else:
+        print(f"{SIDValue} not found in the JSON data.")
+
+    if TSValue is not None:
+        TSValue = TSValue["value"]
+    else:
+        print(f"{TSValue} not found in the JSON data.")
+
+    if CCValue is not None:
+        CCValue = CCValue["value"]
+    else:
+        print(f"{CCValue} not found in the JSON data.")
+
+    cookie_dict = {
+        "__Secure-1PSID": SIDValue ,
+        "__Secure-1PSIDTS": TSValue,
+        "__Secure-1PSIDCC": CCValue,
+    }
+
+    return cookie_dict
+
+cookie_dict = CookieScrapper()
 
 bard = BardCookies(cookie_dict=cookie_dict)
 
+def split_and_save_paragraphs(data, filename):
+    paragraphs = data.split('\n\n')
+    with open(filename, 'w') as file:
+        file.write(data)
+    data = paragraphs[:2]
+    separator = ', '
+    joined_string = separator.join(data)
+    return joined_string
+
+#for getting summary
 while True:
-    Query = input("Enter your Query: ")
-    Reply = bard.get_answer(Query)['content']
-    print(Reply)
+    Question = input("Enter The Query : ")
+    RealQuestion = str(Question)
+    results = bard.get_answer(RealQuestion)['content']
+    current_datetime = datetime.datetime.now()
+    formatted_time = current_datetime.strftime("%H%M%S")
+    filenamedate = str(formatted_time) + str(".txt")
+    filenamedate = "Brain\\DataBase\\" + filenamedate
+    print(split_and_save_paragraphs(results, filename=filenamedate))
